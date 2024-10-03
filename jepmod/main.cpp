62bb76e40f37d1bfdfa57f7f6eff9cc7faa931a1
Paul PARISOT <paul.parisot@epitech.eu>
2024-10-03 11:24:25 +0200

    feat(module): read modules

--- jepmod/main.cpp
+++ jepmod/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "jepmod/DLLoader.hpp"
+#include "jepmod/Jepmodule.hpp"
 
 int main(void)
 {
-    jmod::DLLoader loader("./caca.so");
-    auto foo = loader.getFunc<int>("foo");
+    // jmod::DLLoader loader("./caca.so");
+    // auto foo = loader.getFunc<int>("foo");
 
-    std::cout << foo() << std::endl;
+    // std::cout << foo() << std::endl;
+
+    jmod::Jepmodule m("basic");
     return 0;
 }
