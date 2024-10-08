00257b81caf54a515926264bf9860114811391c2
John DE KETTELBUTTER <john.de-kettelbutter@epitech.eu>
2024-10-08 15:07:30 +0200

    feat(build): Cross platform compilation via bash script

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-if [[ $1 == "fclean" ]] || [[ $1 == "re" ]]; then
-    rm -rf build r-type_server r-type_client;
-fi
+## This script can be used to build on Linux.
+## You need to have CMake installed.
+## If you want to build on Windows, you need to install a bash interpreter like Git Bash.
 
-if [[ $1 == "fclean" ]]; then
-    exit 0 ;
+if [[ $1 == "clean" ]]; then
+    echo -e "\e[33mCleaning build directory.\e[0m"
+    rm -f r-type
+    if [[ $? -ne 0 ]]; then
+        echo -e "\e[31mFailed to remove r-type file.\e[0m"
+        exit 1
+    else 
+        echo -e "\e[32mr-type file removed.\e[0m"
+    fi
+    exit 0
 fi
 
 if [[ -d "build" ]]; then
@@ -27,26 +35,29 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
-echo -e "\e[33mLaunching R-TYPE build.\e[0m"
+echo -e "\e[33mLaunching cmake.\e[0m"
 cmake ../;
 if [[ $? -ne 0 ]]; then
     echo -e "\e[31mFailed to run cmake.\e[0m"
     exit 1
 fi
-echo -e "\e[32mMakefile created.\e[0m"
+echo -e "\e[32mCMake finished.\e[0m"
 
-echo -e "\e[33mLaunching make.\e[0m"
-make ;
+echo -e "\e[33mBuilding targets.\e[0m"
+cmake --build .
 if [[ $? -ne 0 ]]; then
-    echo -e "\e[31mFailed to run make.\e[0m"
+    echo -e "\e[31mFailed to build targets.\e[0m"
     exit 1
 fi
-echo -e "\e[32mMake done.\e[0m"
+echo -e "\e[32mTargets built.\e[0m"
 
 echo -e "\e[33mCopying files.\e[0m"
-cp r-type* ../
-# cp libjepgo.a ../
-# cp libjepgame.a ../
+unameOut="$(uname -o)"
+if [[ $unameOut == "Msys" ]]; then
+    cp Debug/r-type.exe ../
+elif [[ $unameOut == "GNU/Linux" ]]; then
+    cp r-type ../
+fi
 if [[ $? -ne 0 ]]; then
     echo -e "\e[31mFailed to copy files.\e[0m"
     exit 1
