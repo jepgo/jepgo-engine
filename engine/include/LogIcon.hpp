d352ed7591db57cc41c3e3b18e1321df2d651552
Esteban <esteban.marques@epitech.eu>
2024-10-08 17:48:05 +0200

    fix(game menu): handling resize on the login icon

--- engine/include/LogIcon.hpp
+++ engine/include/LogIcon.hpp
@@ -29,6 +29,8 @@ namespace Menu {
              * @param y The y position of the icon
              * @param width The width of the icon
              * @param height The height of the icon
+             * @param screenWidth The width of the screen
+             * @param screenHeight The height of the screen
              * @param text The text of the icon
              * @param font The font of the icon
              */
@@ -37,6 +39,8 @@ namespace Menu {
                 const size_t y,
                 const size_t width,
                 const size_t height,
+                const float screenWidth,
+                const float screenHeight,
                 const std::string text,
                 Font &font
             );
@@ -60,6 +64,14 @@ namespace Menu {
              * @param text The text of the icon
              */
             void setText(const std::string &text) { _text = text; }
+
+            /**
+             * @brief Update the size of the width
+             * 
+             * @param screenWidth The new width of the screen
+             * @param screenHeight The new height of the screen
+             */
+            void updateSize(const float screenWidth, const float screenHeight);
 
         private:
             /**
@@ -109,6 +121,9 @@ namespace Menu {
              * 
              */
             Texture2D _icon;
+
+            float _screenWidth;
+            float _screenHeight;
 
 
 
