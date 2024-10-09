7167bf6940790f39b17f365a29257d790561a638
John DE KETTELBUTTER <john.de-kettelbutter@epitech.eu>
2024-10-09 16:51:32 +0200

    fix(modules): jepmod to fix

--- jepmod/main.cpp
+++ jepmod/main.cpp
@@ -4,11 +4,11 @@
 
 int main(void)
 {
-    jmod::DLLoader loader("./caca.so");
+    jmod::DLLoader loader("C:\\Users\\John\\source\\repos\\R-Type\\build\\Debug\\zizi.dll");
     auto foo = loader.getFunc<int, int, int>("foo");
 
     std::cout << foo(2, 3) << std::endl;
 
-    jmod::Jepmodule m("basic");
+    // jmod::Jepmodule m("basic");
     return 0;
 }
