4642fc12ed284ca479c32dc0217bf5397bfb9adf
Paul PARISOT <paul.parisot@epitech.eu>
2024-10-09 15:50:14 +0200

    fix(modules): dlloader for windows

--- jepmod/DLLoader.cpp
+++ jepmod/DLLoader.cpp
@@ -5,7 +5,11 @@
 ** DLLoader
 */
 
-#include <dlfcn.h>
+#ifdef WINDOWS
+    #include <windows.h>
+#else
+    #include <dlfcn.h>
+#endif
 
 #include "DLLoader.hpp"
 
@@ -13,7 +17,11 @@ using namespace jmod;
 
 DLLoader::DLLoader(std::string const &filename)
 {
+#ifdef WINDOWS
+    _ptr = LoadLibrary(TEXT(filename.c_str()));
+#else
     _ptr = dlopen(filename.c_str(), RTLD_LAZY);
+#endif
     if (_ptr == nullptr)
         throw std::runtime_error(dlerror());
 }
