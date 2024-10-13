1ec38098d5e9f23a685dee388b17438e881beb92
Paul PARISOT <paul.parisot@epitech.eu>
2024-10-13 12:20:57 +0200

    docs(modules): add commented docs for doxygen

--- jepmod/DLLoader.hpp
+++ jepmod/DLLoader.hpp
@@ -33,11 +33,52 @@
 #endif
 
 namespace jmod {
+    /**
+     * Load your shared library using DLLoader class.
+     * 
+     * You will first have to give the shared library path, then you can use
+     * the `getFunc` method to retrieve your functions.
+     */
     class DLLoader {
         public:
-            DLLoader(std::string);
+            /**
+             * This is the DLLoader constructor.
+             * 
+             * ### Arguments
+             * `filePath` - The path to your library.
+             * 
+             * ### Throws
+             * `std::runtime_error` - If the file can not be opened.
+             */
+            DLLoader(std::string filePath);
+
+            /**
+             * This is the DLLoader destroyer. Nothing to say about it...
+             */
             ~DLLoader();
 
+            /**
+             * Get a function in a shared library.
+             * 
+             * Please note that you have to know what your function returns
+             * and what arguments it takes.
+             * 
+             * ### Templates
+             * `A` - The return type.
+             * `B...` - The argument types.
+             * 
+             * ### Arguments
+             * `sym` - The symbol, aka your function name
+             * 
+             * ### Returns
+             * A function pointer to what you asked
+             * 
+             * ### Example
+             * `lib.getFunc<int, int, int>("add");`
+             * 
+             * This will get a function named "add" that returns a int, and
+             * takes 2 ints in parametter.
+             */
             template <typename A, typename... B>
             inline auto getFunc(std::string const &sym) -> A(*)(B...) {
                 auto access = reinterpret_cast<A(*)(B...)>(
---
+++ jepmod/README.md
@@ -0,0 +1,9 @@
+<h1> JEPMOD Collection </h1>
+
+Jepmod contains everything you need for external code, exportation and modules.
+
+For example, everything that deals wuth dynamic linking (`.so` or `.dll` files)
+can be found here.
+
+It also contains the `openinclude` collection, that generates `.so` for
+components exportation when you deal with your own components.
