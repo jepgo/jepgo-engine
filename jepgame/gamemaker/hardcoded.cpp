fe1c00b6fd3968fa22862f9bc1a14750126e5e0f
Paul PARISOT <paul.parisot@epitech.eu>
2024-10-21 11:03:18 +0200

    chore: i dont remmeber

--- jepgame/gamemaker/hardcoded.cpp
+++ jepgame/gamemaker/hardcoded.cpp
@@ -30,52 +30,53 @@ void jgame::hardcodedSystems(EitherRef<Client, Server> something)
             }
         } else {
             base.systems.move.system(base.ecs, base.getTime());
-            AttachModuleSystem::system(base.ecs);
-            MoveToPlayerTimeSystem::system(base.ecs, base.getTime());
-            Animation2TimeSystem::system(base.ecs, base.getTime());
-            ShortAnimationSystem::system(base.ecs, base.getTime());
-            InvinsibleSystem::system(base.ecs, base.getTime());
-            ModuleSystem::system(base.ecs);
-            DmgSystem::system(base.ecs, base.getTime());
+            // DmgSystem::system(base.ecs, base.getTime());
             // ExplosionSystem::system(base.ecs); TODO: update to use with sounds
-            BombGenerationTimeSystem::system(base.ecs, base.getTime());
-            BombGenerationSystem::system(base.ecs, base.getTime());
             // DeathSystem::system(base.ecs, playerEntity);
             // SoundLoopSystem::system(base.ecs, sounds, base.getTime());
             // DetachModulesSystem::system(base.ecs, base.getTime(), key);
-            AttachModuleSystem::system(base.ecs);
             // SystemGame.system(base.ecs, base.getTime(), playerEntity);
             // LoopMoveSystem::system(base.ecs, height, width);
-            MoveToPlayerTimeSystem::system(base.ecs, base.getTime());
-            AutoShootSystem::system(base.ecs, base.getTime());
             // SystemMoveTo.system(base.ecs, base.getTime());
             // movetoplayer.system(base.ecs, base.getTime());
             // moveSys.system(base.ecs, base.getTime());
             Animation2TimeSystem::system(base.ecs, base.getTime());
             // animSys.system(base.ecs, base.getTime());
-            ShortAnimationSystem::system(base.ecs, base.getTime());
-            InvinsibleSystem::system(base.ecs, base.getTime());
-            base.systems.addDmg.system(base.ecs, base.getTime());
-            ModuleSystem::system(base.ecs);
-            DmgSystem::system(base.ecs, base.getTime());
-            ExplosionSystem::system(base.ecs, 3);
             // RebornSystem::system(base.ecs, playerEntity, base.getTime(), key);
             // DestoyersSystem::system(base.ecs, height, width);
             // SoundLoopSystem::system(base.ecs, sounds, base.getTime());
-            BombGenerationTimeSystem::system(base.ecs, base.getTime());
-            BombGenerationSystem::system(base.ecs, base.getTime());
-            base.systems.hit.system(base.ecs);
+
+            // AttachModuleSystem::system(base.ecs);
+            // MoveToPlayerTimeSystem::system(base.ecs, base.getTime());
+            // Animation2TimeSystem::system(base.ecs, base.getTime());
+            // ShortAnimationSystem::system(base.ecs, base.getTime());
+            // InvinsibleSystem::system(base.ecs, base.getTime());
+            // ModuleSystem::system(base.ecs);
+            // BombGenerationTimeSystem::system(base.ecs, base.getTime());
+            // BombGenerationSystem::system(base.ecs, base.getTime());
+            // MoveToPlayerTimeSystem::system(base.ecs, base.getTime());
+            // AutoShootSystem::system(base.ecs, base.getTime());
+            // ShortAnimationSystem::system(base.ecs, base.getTime());
+            // InvinsibleSystem::system(base.ecs, base.getTime());
+            // AttachModuleSystem::system(base.ecs);
+            // BombGenerationTimeSystem::system(base.ecs, base.getTime());
+            // BombGenerationSystem::system(base.ecs, base.getTime());
+            // ModuleSystem::system(base.ecs);
+            // DmgSystem::system(base.ecs, base.getTime());
+            // ExplosionSystem::system(base.ecs, 3);
+            // base.systems.addDmg.system(base.ecs, base.getTime());
+            // base.systems.hit.system(base.ecs);
+            // MessageTimeSystem::system(base.ecs, base.getTime());
 
             std::optional<jgo::ConnectionRef> client = base.getFirstClient();
             if (client) {
                 jgo::Connection &ref = client->get();
                 try {
                     int n = std::any_cast<int>(ref.storage["id"]);
-                    DeathSystem::system(base.ecs, n);
-                    base.systems.test.Stages(base.ecs, base.getTime(), n);
+                    // DeathSystem::system(base.ecs, n);
+                    // base.systems.test.Stages(base.ecs, base.getTime(), n);
                 } catch(std::exception const &) {}
             }
-            MessageTimeSystem::system(base.ecs, base.getTime());
         }
     }, something);
 }
