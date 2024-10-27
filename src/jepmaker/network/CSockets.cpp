a67473a96a2169263064f37937d79c2adf6ffb10
Paul PARISOT <paul.parisot@epitech.eu>
2024-10-27 16:45:37 +0100

    feat(network): add the asio implementation

---
+++ src/jepmaker/network/CSockets.cpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2024
+** rtype
+** File description:
+** Sockets
+*/
+
+#include <sys/socket.h>
+#include <netinet/
+#include "jepengine/types.hpp"
+
+/**
+ * The encapsulation of C Sockets.
+ */
+class CSockets {
+    public:
+        CSockets(jgo::u8 port) {
+
+        }
+    private:
+
+};
+
