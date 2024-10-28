f9fd6898242b063278596c8bf79a6ad6e60692e5
Paul PARISOT <paul.parisot@epitech.eu>
2024-10-28 23:21:53 +0100

    fix: change magic number

--- include/jepmaker/network/INetwork.hpp
+++ include/jepmaker/network/INetwork.hpp
@@ -16,8 +16,9 @@
 namespace jgo {
 
     static int const BUFFER_SIZE = 1024;
-    static std::string const MAGIC_START = "*start*";
-    static std::string const MAGIC_END = "*end*";
+    static jgo::u32 const MAGIC_NUMBER = 0x697a697a;
+    static std::string const MAGIC_START(reinterpret_cast<char const *>(&MAGIC_NUMBER), 4);
+    static std::string const MAGIC_END(reinterpret_cast<char const *>(&MAGIC_NUMBER), 4);
 
     /**
      * A connection is a wrapper around a Port and an IP
@@ -40,7 +41,36 @@ namespace jgo {
      */
     using NetMessage = std::pair<std::string, std::shared_ptr<IConnection>>;
 
-    class INetwork {
+    class IServer {
+        public:
+            /**
+             * Host a server.
+             * 
+             * It should begin listening on the specified port.
+             */
+            virtual void
+            host(jgo::u16 port) = 0;
+
+            /**
+             * Stop your server.
+             * 
+             * It should free the ressources (if needed) and stop connections.
+             */
+            virtual void
+            stop(void) = 0;
+
+            /**
+             * Get all messages.
+             * 
+             * A message is a duo variable containing a string and a shared
+             * pointer to a IConnection.
+             */
+            virtual std::vector<NetMessage>
+            getAllMessages(void) = 0;
+            
+    };
+
+    class IClient {
         public:
             /**
              * Host a server.
