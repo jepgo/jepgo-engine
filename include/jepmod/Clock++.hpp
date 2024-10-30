5ff94b38849db50984887d97fd86047863cb7567
Paul PARISOT <paul.parisot@epitech.eu>
2024-10-30 17:32:23 +0100

    fix: clock with std chrono

--- include/jepmod/Clock++.hpp
+++ include/jepmod/Clock++.hpp
@@ -8,39 +8,43 @@
 #pragma once
 
 #include <ctime>
+#include <chrono>
 
 namespace jmod {
     class ClockPP {
         public:
-            ClockPP(void) noexcept : _clock(std::clock()) {
+            ClockPP(void) noexcept : _start(std::chrono::high_resolution_clock::now()), _point(_start) {
                 return;
             }
-            ClockPP(std::clock_t c) noexcept : _clock(c) {
-                return;
+            // ClockPP(std::clock_t c) noexcept : _clock(c) {
+            //     return;
+            // }
+            // inline explicit operator float(void) const noexcept {
+            //     return static_cast<float>(std::chrono::steady_clock::to_time_t(this->_clock));
+            // }
+            inline double get() {
+                return std::chrono::duration<double>(_point - _start).count();
             }
-            constexpr inline explicit operator float(void) const noexcept {
-                return static_cast<float>(_clock) /
-                    static_cast<float>(CLOCKS_PER_SEC);
-            }
-            inline ClockPP operator-(ClockPP const &other) const noexcept {
-                return ClockPP(this->_clock - other._clock);
-            }
-            inline ClockPP operator+(ClockPP const &other) const noexcept {
-                return ClockPP(this->_clock + other._clock);
-            }
-            inline ClockPP &operator-=(ClockPP const &other) noexcept {
-                this->_clock -= other._clock;
-                return *this;
-            }
-            inline ClockPP &operator+=(ClockPP const &other) noexcept {
-                this->_clock += other._clock;
-                return *this;
-            }
+            // inline ClockPP operator-(ClockPP const &other) const noexcept {
+            //     return ClockPP(this->_clock - other._clock);
+            // }
+            // inline ClockPP operator+(ClockPP const &other) const noexcept {
+            //     return ClockPP(this->_clock + other._clock);
+            // }
+            // inline ClockPP &operator-=(ClockPP const &other) noexcept {
+            //     this->_clock -= other._clock;
+            //     return *this;
+            // }
+            // inline ClockPP &operator+=(ClockPP const &other) noexcept {
+            //     this->_clock += other._clock;
+            //     return *this;
+            // }
             inline ClockPP &operator()(void) noexcept {
-                this->_clock = std::clock();
+                this->_point = std::chrono::high_resolution_clock::now();
                 return *this;
             }
         private:
-            std::clock_t _clock;
+            std::chrono::high_resolution_clock::time_point _start;
+            std::chrono::high_resolution_clock::time_point _point;
     };
 }
