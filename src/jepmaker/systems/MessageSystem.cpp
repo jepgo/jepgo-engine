ec8b53aafabc352188bda29ace69b91a4298ce2f
Paul PARISOT <paul.parisot@epitech.eu>
2024-10-31 14:31:22 +0100

    fix(game): gregoire's warnings

--- src/jepmaker/systems/MessageSystem.cpp
+++ src/jepmaker/systems/MessageSystem.cpp
@@ -5,7 +5,6 @@
 ** MessageSystem
 */
 
-#pragma once
 #include "jepengine/Client.hpp"
 #include "jepmod/exported.hpp"
 #include "jepmaker/components/Message.hpp"
@@ -16,7 +15,18 @@ exported(void) jepgoSystem(jgo::Game &game, float time)
 
     for (std::size_t i = 0; i < mess.size(); i++) {
         if (mess[i].has_value()) {
-            game.getGraphicLib()->drawText(mess[i].value()._mess.c_str(), jgo::Rectangle {(int)mess[i].value()._pos.x, (int)mess[i].value()._pos.y, 0, 0},  mess[i].value()._color, "", mess[i].value()._size);
+            game.getGraphicLib()->drawText(
+                mess[i].value()._mess.c_str(),
+                jgo::Rectangle {
+                    mess[i].value()._pos.x,
+                    mess[i].value()._pos.y,
+                    0.0f,
+                    0.0f
+                },
+                mess[i].value()._color,
+                "",
+                mess[i].value()._size
+            );
         }
     }
 };
